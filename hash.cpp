674a28ce0e8153adbb1dfae7c3a59173d5eaed74
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-04 15:05:21 +0530

    Add existing DSA tracker files

--- hash.cpp
+++ hash.cpp
@@ -94,3 +94,26 @@ public:
         return {};
     }
 };
+//  IMPORTANT: This solution assumes that there is exactly one solution, and you may not use the same element twice.
+// Problem: Group Anagrams
+// LeetCode: https://leetcode.com/problems/group-anagrams/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramGroups;
+        
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            anagramGroups[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto& entry : anagramGroups) {
+            result.push_back(entry.second);
+        }
+
+        return result;
+    }
+};
