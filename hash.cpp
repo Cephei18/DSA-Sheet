ca9ffb8229977c52aaa3560f708685ded14c7c8f
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-13 12:04:17 +0530

    Add existing DSA tracker files

--- hash.cpp
+++ hash.cpp
@@ -207,3 +207,26 @@ public:
         return true;
     }
 };
+
+// Problem: Find the Town Judge
+// LeetCode: https://leetcode.com/problems/find-the-town-judge/
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> delta(n + 1, 0);
+        
+        for (auto& t : trust) {
+            delta[t[0]]--;
+            delta[t[1]]++;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if (delta[i] == n - 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
