7f81de4837566aa2c3e3c3ac0844e37b1f55b2fb
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-14 16:09:41 +0530

    Add existing DSA tracker files

--- set.cpp
+++ set.cpp
@@ -42,3 +42,27 @@ public:
 // return false;
 
 // If the loop completes without finding any duplicate, return false.
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Problem: Longest Consecutive Sequence
+// LeetCode: https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (numSet.find(num - 1) == numSet.end()) {
+                int length = 1;
+                while (numSet.find(num + length) != numSet.end()) {
+                    length++;
+                }
+                longest = max(longest, length);
+            }
+        }
+        return longest;
+    }
+};
