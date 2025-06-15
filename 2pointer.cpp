9d5da5ffb772154539a3e9a1ca5bd09035bcaaf2
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-15 16:22:57 +0530

    Add existing DSA tracker files

--- 2pointer.cpp
+++ 2pointer.cpp
@@ -163,3 +163,25 @@ public:
         return maxP;
     }
 };
+
+// Problem: Longest Substring Without Repeating Characters
+// LeetCode: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> charSet;
+        int l = 0;
+        int res = 0;
+
+        for (int r = 0; r < s.size(); r++) {
+            while (charSet.find(s[r]) != charSet.end()) {
+                charSet.erase(s[l]);
+                l++;
+            }
+            charSet.insert(s[r]);
+            res = max(res, r - l + 1);
+        }
+        return res;
+    }
+};
