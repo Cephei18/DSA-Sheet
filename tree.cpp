7229fbda00b19ff5f314c3eba05663fa76efae20
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-21 20:04:46 +0530

    Add existing DSA tracker files

--- tree.cpp
+++ tree.cpp
@@ -48,3 +48,53 @@ TreeNode* insert(TreeNode* root, int val) {
     return root;
 }
 
+// Problem: Delete Node in a BST
+// LeetCode: https://leetcode.com/problems/delete-node-in-a-bst/
+
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+
+        // Step 1: Traverse to find the node
+        if (key < root->val_) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val_) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            // Found the node to delete
+
+            // Case 1: No child
+            if (!root->left && !root->right) {
+                delete root;
+                return nullptr;
+            }
+
+            // Case 2: One child
+            else if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            } else if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            // Case 3: Two children
+            TreeNode* successor = findMin(root->right);
+            root->val_ = successor->val_;  // Copy value
+            root->right = deleteNode(root->right, successor->val_);  // Delete the duplicate
+        }
+
+        return root;
+    }
+
+private:
+    TreeNode* findMin(TreeNode* node) {
+        while (node && node->left) {
+            node = node->left;
+        }
+        return node;
+    }
+};
