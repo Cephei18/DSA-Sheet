68a408049c02613c0a61f0672521d51f20b144e1
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-23 17:56:46 +0530

    Add existing DSA tracker files

--- tree.cpp
+++ tree.cpp
@@ -145,3 +145,35 @@ public:
         
     }
 };
+
+// Problem: Subtree of Another Tree
+// LeetCode: https://leetcode.com/problems/subtree-of-another-tree/
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!subRoot) {
+            return true;
+        }
+        if (!root) {
+            return false;
+        }
+
+        if (sameTree(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root->left, subRoot) || 
+               isSubtree(root->right, subRoot);
+    }
+
+    bool sameTree(TreeNode* root, TreeNode* subRoot) {
+        if (!root && !subRoot) {
+            return true;
+        }
+        if (root && subRoot && root->val_ == subRoot->val_) {
+            return sameTree(root->left, subRoot->left) &&
+                   sameTree(root->right, subRoot->right);
+        }
+        return false;
+    }
+};
