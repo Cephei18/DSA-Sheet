82b7c889eb9e06ed84bb6b561513ead2c23c8e22
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-25 20:47:03 +0530

    Add existing DSA tracker files

--- heap.cpp
+++ heap.cpp
@@ -146,3 +146,36 @@ public:
 
     }
 };
+
+// Problem: Find Median from Data Stream
+// LeetCode: https://leetcode.com/problems/find-median-from-data-stream/
+
+class MedianFinder {
+
+private:
+        priority_queue<int>maxHeap;
+        priority_queue<int, vector<int>, greater<int>> minHeap; 
+
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        if(minHeap.size()>maxHeap.size()){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+    if (maxHeap.size() == minHeap.size()) {
+        return (maxHeap.top() + minHeap.top()) / 2.0;
+    } 
+    else {
+        return maxHeap.top();
+    }
+}
+};
