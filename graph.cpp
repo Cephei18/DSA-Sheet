d98724484dae7770a32d0cf1e67768d2daaaa923
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-27 12:33:16 +0530

    Add existing DSA tracker files

--- graph.cpp
+++ graph.cpp
@@ -593,3 +593,42 @@ public:
         }
      }
 };
+
+// Problem: Course Schedule II
+// LeetCode: https://leetcode.com/problems/course-schedule-ii/
+
+class Solution {
+    vector<int> output;
+    vector<int> indegree;
+    vector<vector<int>> adj;
+    
+    void dfs(int node) {
+        output.push_back(node);
+        indegree[node]--;
+        for (int nei : adj[node]) {
+            indegree[nei]--;
+            if (indegree[nei] == 0) {
+                dfs(nei);
+            }
+        }
+    }
+
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        adj = vector<vector<int>>(numCourses);
+        indegree = vector<int>(numCourses, 0);
+        for (auto& pre : prerequisites) {
+            indegree[pre[0]]++;
+            adj[pre[1]].push_back(pre[0]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                dfs(i);
+            }
+        }
+
+        if (output.size() != numCourses) return {};
+        return output;
+    }
+};
