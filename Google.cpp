71cfa0be5cf6a4e8347064244d00000db696b004
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-27 12:54:10 +0530

    Add existing DSA tracker files

---
+++ Google.cpp
@@ -0,0 +1,61 @@
+// This sheet will have all the problems that have been asked in the Google interview.
+//  We will cover problems related to graphs, trees, and dynamic programming.
+// First mention problem name and then its link.
+
+// Topic - Graphs 
+
+// Problem: Number of Islands
+// LeetCode: https://leetcode.com/problems/number-of-islands/
+
+// Problem: Valid Tree
+// LeetCode: https://leetcode.com/problems/valid-tree/
+
+// Problem: Clone Graph
+// LeetCode: https://leetcode.com/problems/clone-graph/
+
+// Problem: Pacific Atlantic Water Flow
+// LeetCode: https://leetcode.com/problems/pacific-atlantic-water-flow/
+
+// Problem: Surrounded Regions
+// LeetCode: https://leetcode.com/problems/surrounded-reg
+
+// Problem: Island Perimeter
+// LeetCode: https://leetcode.com/problems/island-perimeter/
+
+// Problem: Max Area of Island
+// LeetCode: https://leetcode.com/problems/max-area-of-island/
+
+// Problem: Course Schedule
+// LeetCode: https://leetcode.com/problems/course-schedule/
+
+// Problem: Find the Maximum Number of Fish in a Pond
+// LeetCode: https://leetcode.com/problems/find-the-maximum-number-of-fish-in-a-pond/
+
+// Problem: Course Schedule II
+// LeetCode: https://leetcode.com/problems/course-schedule-ii/
+
+// Problem: Number of Connected Components in an Undirected Graph
+// LeetCode: https://leetcode.com/problems/number-of-connected-components-in-an-undirected
+
+// Problem: Find Eventual Safe States
+// LeetCode: https://leetcode.com/problems/find-eventual-safe-states/
+
+// Problem: Find the Town Judge
+// LeetCode: https://leetcode.com/problems/find-the-town-judge/
+
+
+// Problem: Find the Redundant Connection
+// LeetCode: https://leetcode.com/problems/find-the-redundant-connection/
+
+// Problem: Find the Redundant Directed Connection
+// LeetCode: https://leetcode.com/problems/find-the-redundant-directed-connection
+
+// Problem: Find the Minimum Height Trees
+// LeetCode: https://leetcode.com/problems/find-minimum-height-trees/
+
+// Problem: Find the Longest Path in a Directed Acyclic Graph
+// LeetCode: https://leetcode.com/problems/find-the-longest-path-in-a-directed-acyclic-graph/
+
+// Problem: Find the Shortest Path in a Grid with Obstacles Elimination
+// LeetCode: https://leetcode.com/problems/find-the-shortest-path-in-a-grid-withobstacles-elimination/
+
