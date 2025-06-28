03ac7d7c83e5679f160a7b84265e1c20ed6baff7
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-28 16:22:17 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -20,3 +20,20 @@ public:
         return dp[n];
     }
 };
+
+// Problem: House Robber
+// LeetCode: https://leetcode.com/problems/house-robber/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int rob1 = 0, rob2 = 0;
+
+        for (int num : nums) {
+            int temp = max(num + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        return rob2;
+    }
+};
