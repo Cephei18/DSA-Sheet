849be8bc9a2ff12fdf10dd867d5d5d6bf09bba34
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-06-30 13:55:48 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -90,3 +90,37 @@ public:
         return prev[0];
     }
 };
+
+// Problem: Coin Change
+// LeetCode: https://leetcode.com/problems/coin-change/
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+       if(amount == 0) return 0;
+
+       queue<int> q;
+       q.push(0);
+       vector<bool>seen(amount+1,false);
+
+       seen[0] = true;
+       int res = 0;
+
+       while(!q.empty()){
+        res++;
+        int size = q.size();
+        for(int i=0;i<size;i++){
+            int cur = q.front();
+            q.pop();
+            for(int coin:coins){
+                int nxt = cur + coin;
+                if(nxt == amount) return res;
+                if(nxt>amount || seen[nxt]) continue;
+                seen[nxt] = true;
+                q.push(nxt);
+            }
+        }
+       }
+       return -1;
+    }
+};
