a511868ca3f04d04a4a36f4663880a7ee50d2314
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-03 17:21:17 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -229,3 +229,22 @@ public:
         return res;
     }
 };
+
+// Problem: Unique Paths
+// LeetCode: https://leetcode.com/problems/unique-paths/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> row(n, 1);
+
+        for (int i = 0; i < m - 1; ++i) {
+            vector<int> newRow(n, 1);
+            for (int j = n - 2; j >= 0; --j) {
+                newRow[j] = newRow[j + 1] + row[j];
+            }
+            row = newRow;
+        }
+        return row[0];
+    }
+};
