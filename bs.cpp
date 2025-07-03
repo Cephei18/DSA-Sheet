545d1701e7abd3db98e4cd024a80d13e035fdfe1
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-03 19:53:32 +0530

    Add existing DSA tracker files

---
+++ bs.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem: Search in Rotated Sorted Array
+// LeetCode: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       int l=0, r= nums.size()-1;
+
+       while(l<=r){
+        int mid = (l+r)/2;
+        if(target == nums[mid]){
+            return mid;
+        }
+
+        if(nums[l] <= nums[mid]){
+            if(target> nums[mid] || target< nums[l]){
+                l = mid + 1;
+            }
+            else{
+                r = mid -1;
+            }
+        }
+        else{
+        if(target< nums[mid] || target > nums[r]){
+            r = mid -1;
+        }
+        else{
+            l = mid + 1;
+        }
+        }
+       } 
+    return -1;
+    }
+};
