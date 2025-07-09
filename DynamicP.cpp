687e7a41deda1f65d32b2ed0dc397314d43da0df
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-09 23:42:35 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -248,3 +248,21 @@ public:
         return row[0];
     }
 };
+
+// Problem: Jump Game
+// LeetCode: https://leetcode.com/problems/jump-game/
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size() - 1;
+
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+};
