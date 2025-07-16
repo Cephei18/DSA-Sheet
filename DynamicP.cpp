daf6f08200b61b1d3398c7354239fd611b7023ef
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-16 22:30:29 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -344,6 +344,21 @@ public:
     }
 };
 
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return backtrack(0, 0, nums, target);
+    }
+    
+    int backtrack(int i, int total, vector<int>& nums, int target) {
+        if (i == nums.size()) {
+            return total == target;
+        }
+        return backtrack(i + 1, total + nums[i], nums, target) + 
+               backtrack(i + 1, total - nums[i], nums, target);
+    }
+};
+
 // Problem: Find Maximum Number of Strings That Can Be Formed
 // LeetCode: https://leetcode.com/problems/find-maximum-number-of-strings-that-can-be-formed/
 
@@ -371,3 +386,4 @@ public:
         return dp[m][n];
     }
 };
+
