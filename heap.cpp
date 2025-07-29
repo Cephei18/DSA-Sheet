837742e45068206f5dacdba911e8e4ac90a2da92
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-29 19:12:03 +0530

    Add existing DSA tracker files

--- heap.cpp
+++ heap.cpp
@@ -179,3 +179,23 @@ public:
     }
 }
 };
+
+// Problem: Meeting Rooms II
+// LeetCode: https://leetcode.com/problems/meeting-rooms-ii/
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a.start < b.start;
+        });
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (const auto& interval : intervals) {
+            if (!minHeap.empty() && minHeap.top() <= interval.start) {
+                minHeap.pop();
+            }
+            minHeap.push(interval.end);
+        }
+        return minHeap.size();
+    }
+};
