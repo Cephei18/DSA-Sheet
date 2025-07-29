d0e3351c4f1dce55223fb4fe888f71e9d0b0367f
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-29 16:46:34 +0530

    Add existing DSA tracker files

--- math.cpp
+++ math.cpp
@@ -143,3 +143,28 @@ public:
         return true;
     }
 };
+
+// Problem: Spiral Matrix
+// LeetCode: https://leetcode.com/problems/spiral-matrix/
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, 
+                                             {0, -1}, {-1, 0}};
+        vector<int> steps = {matrix[0].size(), matrix.size() - 1};
+
+        int r = 0, c = -1, d = 0;
+        while (steps[d % 2]) {
+            for (int i = 0; i < steps[d % 2]; i++) {
+                r += directions[d].first;
+                c += directions[d].second;
+                res.push_back(matrix[r][c]);
+            }
+            steps[d % 2]--;
+            d = (d + 1) % 4;
+        }
+        return res;
+    }
+};
