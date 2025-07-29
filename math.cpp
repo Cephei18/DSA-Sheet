386cf91b5107db281e208e7d324d159a294d8ade
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-29 18:22:55 +0530

    Add existing DSA tracker files

--- math.cpp
+++ math.cpp
@@ -168,3 +168,22 @@ public:
         return res;
     }
 };
+
+// Problem: Meeting Rooms
+// LeetCode: https://leetcode.com/problems/meeting-rooms/
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+       sort(intervals.begin(),intervals.end(),[](auto& x, auto& y){
+        return x.start < y.start;
+       });
+       for(int i=1;i<intervals.size();++i){
+        if(intervals[i].start<intervals[i-1].end){
+            return false;
+        }
+       }
+       return true;
+        
+    }
+};
