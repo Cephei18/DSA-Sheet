bb493a4c5e7795e03f67e4f8474c674e196e6637
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-29 18:36:58 +0530

    Add existing DSA tracker files

--- math.cpp
+++ math.cpp
@@ -212,3 +212,28 @@ public:
         return res;
     }
 };
+
+// Problem: Merge Intervals
+// LeetCode: https://leetcode.com/problems/merge-intervals/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> output;
+        output.push_back(intervals[0]);
+
+        for (auto& interval : intervals) {
+            int start = interval[0];
+            int end = interval[1];
+            int lastEnd = output.back()[1];
+
+            if (start <= lastEnd) {
+                output.back()[1] = max(lastEnd, end);
+            } else {
+                output.push_back({start, end});
+            }
+        }
+        return output;
+    }
+};
