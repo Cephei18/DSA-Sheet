5fafec672023a296d7b8eebf361ad2ee724e1f92
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-31 23:18:32 +0530

    Add existing DSA tracker files

--- tree.cpp
+++ tree.cpp
@@ -198,6 +198,30 @@ public:
 };
 
 
+// Problem: Binary Tree Level Order Traversal
+// LeetCode: https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        dfs(root, 0);
+        return res;
+    }
+    
+    void dfs(TreeNode* node, int depth) {
+        if (!node) return;
+        
+        if (res.size() == depth) {
+            res.push_back(vector<int>());
+        }
+        
+        res[depth].push_back(node->val);
+        dfs(node->left, depth + 1);
+        dfs(node->right, depth + 1);
+    }
+};
 
 
 
