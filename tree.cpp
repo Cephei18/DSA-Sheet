c085b457e74bb02959f2de1ff2fa483ee9802178
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-07-31 23:00:38 +0530

    Add existing DSA tracker files

--- tree.cpp
+++ tree.cpp
@@ -177,3 +177,34 @@ public:
         return false;
     }
 };
+
+// Problem: Lowest Common Ancestor of a Binary Search Tree
+// LeetCode: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || !p || !q) {
+            return nullptr;
+        }
+        if (max(p->val, q->val) < root->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else if (min(p->val, q->val) > root->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else {
+            return root;
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
