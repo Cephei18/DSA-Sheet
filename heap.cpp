c9dff992f234513e46ce811f58bef75f2b5dd65f
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-05 19:18:22 +0530

    Add existing DSA tracker files

--- heap.cpp
+++ heap.cpp
@@ -199,3 +199,81 @@ public:
         return minHeap.size();
     }
 };
+
+
+//Problem : Kruskal's Algorithm for Minimum Spanning Tree
+// LeetCode: https://leetcode.com/problems/kruskals-algorithm-for-minimum-spanning-tree/
+// Note: This implementation uses a Union-Find data structure to manage connected components.
+
+class UnionFind {
+public:
+    vector<int> parent;
+    vector<int> size;
+    int numComponents;
+
+    UnionFind(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        numComponents = n;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x) {
+        // Finds the root of x
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    // Union is a reserved keyword in C++, so we use _union instead
+    bool _union(int x, int y) {
+        // Connects x and y
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (size[rootX] < size[rootY]) {
+                parent[rootX] = rootY;
+                size[rootY] += size[rootX];
+            } else {
+                parent[rootY] = rootX;
+                size[rootX] += size[rootY];
+            }
+            numComponents--;
+            return true;
+        }
+        return false;
+    }
+
+    int getNumComponents() {
+        return numComponents;
+    }
+};
+
+class Solution {
+public:
+    // Implementation for Kruskal's algorithm to compute Minimum Spanning Trees
+    int minimumSpanningTree(vector<vector<int>>& edges, int n) {
+        // Sort edges by weight using min heap
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> minHeap; 
+        for (const auto& edge : edges) {
+            minHeap.push(make_tuple(edge[2], edge[0], edge[1]));
+        }
+
+        UnionFind unionFind(n);
+        int totalWeight = 0;
+        while (!minHeap.empty()) {
+            tuple<int, int, int> cur = minHeap.top();
+            int w1 = std::get<0>(cur), n1 = std::get<1>(cur), n2 = std::get<2>(cur);
+            minHeap.pop();
+
+            if (unionFind._union(n1, n2)) {
+                totalWeight += w1;
+            }
+        }
+        // Return -1 if not all nodes are visited (unconnected graph)
+        return unionFind.getNumComponents() != 1 ? -1 : totalWeight;
+    }
+};
