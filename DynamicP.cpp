f70f7247afe17f9b41b1386139779642176c24ad
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-09 20:12:24 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -645,3 +645,32 @@ public:
         return count;
     }
 };
+
+
+// Problem: Subsets
+// LeetCode: https://leetcode.com/problems/subsets/
+
+
+class Solution {
+public:
+    vector<vector<int>>res;
+
+    void dfs(vector<int>& nums, int start, vector<int>& path){
+
+        res.push_back(path);
+
+        for(int i=start;i<nums.size();++i){
+            path.push_back(nums[i]);
+            dfs(nums,i+1,path);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        res.clear();
+        vector<int>path;
+        dfs(nums,0,path);
+        return res;        
+    }
+};
+  
