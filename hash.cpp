352cc3edbbb8ce84bcf33f9d1010971c93fabe2a
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-10 20:04:45 +0530

    Add existing DSA tracker files

--- hash.cpp
+++ hash.cpp
@@ -338,3 +338,25 @@ public:
         return res;
     }
 };
+
+// Problem: Longest Consecutive Sequence
+// LeetCode: https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int maxcount = 0;
+
+        for (int n : numSet) {
+            if (!numSet.count(n - 1)) {
+                int length = 1;
+                while (numSet.count(n + length)) {
+                    length++;
+                }
+                maxcount = max(maxcount, length);
+            }
+        }
+        return maxcount;
+    }
+};
