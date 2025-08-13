03cd3a34687cbfcc1b567b30c058765d113eea27
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-13 16:12:51 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -949,3 +949,23 @@ public:
         return mem[n];
     }
 };
+
+// Min Cost Climbing Stairs
+// LeetCode: https://leetcode.com/problems/min-cost-climbing-stairs/
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n+1, 0); // dp[i] = min cost to reach step i
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        }
+
+        return dp[n];
+    }
+};
