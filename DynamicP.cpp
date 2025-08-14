d6e3047edbe2e3d52638edaa81ec4581d7f943ee
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-14 17:42:15 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -993,3 +993,28 @@ public:
       return dp[n-1];
     }
 };
+
+// Character Replacement
+// LeetCode: https://leetcode.com/problems/character-replacement/
+
+class Solution {
+public:
+    int characterReplacement(std::string s, int k) {
+        unordered_map<char, int> count;
+        int res = 0;
+
+        int l = 0, maxf = 0;
+        for (int r = 0; r < s.size(); r++) {
+            count[s[r]]++;
+            maxf = max(maxf, count[s[r]]);
+
+            while ((r - l + 1) - maxf > k) {
+                count[s[l]]--;
+                l++;
+            }
+            res = max(res, r - l + 1);
+        }
+
+        return res;
+    }
+};
