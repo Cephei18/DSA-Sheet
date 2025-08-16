579fd4e158ab2e1c27c05cdf4311b2df60a1e805
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-16 10:54:40 +0530

    Add existing DSA tracker files

--- heap.cpp
+++ heap.cpp
@@ -277,3 +277,25 @@ public:
         return unionFind.getNumComponents() != 1 ? -1 : totalWeight;
     }
 };
+
+// Problem: Last Stone Weight
+// LeetCode: https://leetcode.com/problems/last-stone-weight/
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+        priority_queue<int> maxHeap(stones.begin(), stones.end());
+
+        while (maxHeap.size() > 1) {
+            int stone1 = maxHeap.top(); maxHeap.pop();
+            int stone2 = maxHeap.top(); maxHeap.pop();
+
+            if (stone1 != stone2) {
+                maxHeap.push(stone1 - stone2);
+            }
+        }
+
+        return maxHeap.empty() ? 0: maxHeap.top();
+    }
+};
