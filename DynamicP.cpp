4849bfd88ea28b5bad12514d8872870f373776bf
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-17 22:17:38 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -1046,3 +1046,32 @@ public:
         return minHeap.top();
     }
 };
+
+// Problem: Longest Palindromic Substring
+// LeetCode: https://leetcode.com/problems/longest-palindromic-substring
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int resIdx = 0, resLen = 0;
+        int n = s.size();
+
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s[i] == s[j] &&
+                    (j - i <= 2 || dp[i + 1][j - 1])) {
+
+                    dp[i][j] = true;
+                    if (resLen < (j - i + 1)) {
+                        resIdx = i;
+                        resLen = j - i + 1;
+                    }
+                }
+            }
+        }
+
+        return s.substr(resIdx, resLen);
+    }
+};
