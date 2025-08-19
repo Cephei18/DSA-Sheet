1a9a5553ae3b42abb10f4571da4884112067502d
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-19 23:15:30 +0530

    Add existing DSA tracker files

--- math.cpp
+++ math.cpp
@@ -265,4 +265,32 @@ public:
 
         return result;
     }
+// Problem: Koko Eating Bananas
+// LeetCode: https://leetcode.com/problems/koko-eating-bananas/
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = *max_element(piles.begin(), piles.end());
+        int ans = right;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            long long hours = 0;
+            for(int pile : piles) {
+                hours += (pile + mid - 1) / mid; // ceil(pile / mid)
+            }
+
+            if(hours <= h) {
+                ans = mid;
+                right = mid - 1; // try smaller k
+            } else {
+                left = mid + 1; // need larger k
+            }
+        }
+
+        return ans;
+    }
 };
