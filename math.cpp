2b97195d7577255cf708c21eb0ea3661c6e722da
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-20 16:05:47 +0530

    Add existing DSA tracker files

--- math.cpp
+++ math.cpp
@@ -294,3 +294,18 @@ public:
         return ans;
     }
 };
+
+// Problem: Rotate Image
+// LeetCode: https://leetcode.com/problems/rotate-image/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(),matrix.end());
+        for(int i=0;i<matrix.size();++i){
+            for(int j = i+1;j<matrix[i].size();++j){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
