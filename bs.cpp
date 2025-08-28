882936ac3aeb271770c2c48cb5e8ddfa01d0211b
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-28 15:01:21 +0530

    Add existing DSA tracker files

--- bs.cpp
+++ bs.cpp
@@ -35,4 +35,37 @@ public:
     return -1;
     }
 };
-//
+
+// Minimum Path Sum
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+
+        // dp has same structure as triangle
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        dp[0][0] = triangle[0][0]; // start point
+
+        for (int r = 1; r < n; r++) {
+            for (int i = 0; i <= r; i++) {
+                if (i == 0) {
+                    // only one parent (left edge)
+                    dp[r][i] = triangle[r][i] + dp[r-1][i];
+                } 
+                else if (i == r) {
+                    // only one parent (right edge)
+                    dp[r][i] = triangle[r][i] + dp[r-1][i-1];
+                } 
+                else {
+                    // both parents exist
+                    dp[r][i] = triangle[r][i] + min(dp[r-1][i-1], dp[r-1][i]);
+                }
+            }
+        }
+
+        // answer is the minimum in the last row
+        return *min_element(dp[n-1].begin(), dp[n-1].end());
+    }
+};
