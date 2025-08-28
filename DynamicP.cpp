45a4c44ae78f8bfa130b9c280abc6499cdd53433
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-08-28 14:11:50 +0530

    Add existing DSA tracker files

--- DynamicP.cpp
+++ DynamicP.cpp
@@ -1177,3 +1177,33 @@ int solve(int day, int last, vector<vector<int>>& points) {
     }
     return maxi;
 }
+
+// Tabulation
+// Time: O(n*4*3) Space: O(n*4)
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size(); 
+
+        vector<vector<long long>>dp(m,vector<long long>(n,0));
+
+        if(obstacleGrid[0][0]==1) return 0;
+        dp[0][0] = 1;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j] = 0;
+                }
+                else{
+                    if(i>0) dp[i][j] += dp[i-1][j];
+                    if(j>0) dp[i][j] += dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
