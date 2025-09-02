c28a8486656b36f34d3cf05fa444361d6ac32edb
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2025-09-02 21:52:38 +0530

    Add existing DSA tracker files

--- 2pointer.cpp
+++ 2pointer.cpp
@@ -714,3 +714,44 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         nums1[k--] = nums2[j--];
     }
 }
+
+
+class Solution {
+public:
+    vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                if (result.empty() || result.back() != nums1[i])
+                    result.push_back(nums1[i]);
+                i++; j++;
+            }
+            else if (nums1[i] < nums2[j]) {
+                if (result.empty() || result.back() != nums1[i])
+                    result.push_back(nums1[i]);
+                i++;
+            }
+            else {
+                if (result.empty() || result.back() != nums2[j])
+                    result.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        // Add remaining elements
+        while (i < nums1.size()) {
+            if (result.empty() || result.back() != nums1[i])
+                result.push_back(nums1[i]);
+            i++;
+        }
+        while (j < nums2.size()) {
+            if (result.empty() || result.back() != nums2[j])
+                result.push_back(nums2[j]);
+            j++;
+        }
+
+        return result;
+    }
+};
